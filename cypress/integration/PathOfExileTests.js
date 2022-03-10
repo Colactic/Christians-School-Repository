fa4326634db4375dcbb407c55213c07e41d4dfaf
Colactic <Christian.S.Bibby@gmail.com>
2022-03-10 02:08:17 +0100

    A few more tests

--- cypress/integration/PathOfExileTests.js
+++ cypress/integration/PathOfExileTests.js
@@ -7,7 +7,6 @@ describe('Visit pathofexile.com', () => {
 describe('Test the Home button', () => {
     it('Click the Home button and check the URL', () => {
         cy.get('li#nav-home').click()
-//        cy.contains('Home').click()
         cy.url().should('include', 'https://www.pathofexile.com')
     })
 })
@@ -20,23 +19,51 @@ describe('Test the Game button', () => {
     })
 })
 
+/*
 describe('Test the Video button', () => {
     it('Hover the Game button, click Video, and check the URL', () => {
-        cy.contains('Game').trigger('mouseover')
-//        cy.contains('Game').invoke('show')
-//        cy.contains('Videos').should('be.visible')
-//        cy.url().should('include', '/videos')
+       cy.get('li#nav-game').trigger('mouseover')
+       cy.contains('Videos').should('be.visible')
+//       cy.url().should('include', '/videos')
     })
 })
+*/
 
 describe('Swap to trade and back again', () => {
     it('Click the trade button, check url, click back to main site button, and check url again', () => {
         cy.get('li#nav-trade').click()
-        cy. wait(2000)
+        cy. wait(1000)
         cy.url().should('include', '/trade')
-        cy.wait(2000)
+        cy.wait(1000)
         cy.get('div.linkBack').click()
-        cy.wait(2000)
+        cy.wait(1000)
         cy.url().should('include', 'https://www.pathofexile.com')
+    })
+})
+
+describe('Change the langue', () => {
+    it('Click the Brazillian flag, check the URL, and swap back again.', () => {
+        cy.get('span.br-lang').click()
+        cy.url().should('include', 'br.pathofexile.com')
+        cy.get('span.us-lang').click()
+    })
+})
+
+describe('Navigate to patch notes', () => {
+    it('Click on Forum, click on Patch Notes thread and confirm url', () => {
+        cy.get('li#nav-forum').click()
+        cy.get('[data-id=366]').contains('Patch Notes').click()
+//        cy.get('a').eq(24).click()
+        cy.url().should('include', '/forum/view-forum/patch-notes')
+    })
+})
+
+describe('Try to log in and fail', () => {
+    it('Click Log In button, type in fake account details, try to log in and fail', () => {
+        cy.get('a#pcButton').click()
+        cy.get('a.statusItem').eq(0).click()
+        cy.wait(5000)
+        cy.url().should('include', '/login')
+        cy.get("input#login_email").focus()
     })
 })
